6989d3d0657791bc639e4e87742618a3e72162cd
anassbe <anasberrabeh6@gmail.com>
2024-09-18 20:36:30 +0100

    resolve conflict

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import RecruitmentOffers from "./offer_space/recruitment-offers";
-
-export default function Home() {
-  return (
-    <RecruitmentOffers></RecruitmentOffers>
-  );
-}
